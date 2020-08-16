2851c9e705e4d9eb07f330f82a40a123d2d25c03
James <jxbromley@ualr.edu>
2020-08-16 10:41:26 -0500

    Added single note read and updated other routes slightly

--- app.js
+++ app.js
@@ -11,11 +11,18 @@ function createMySqlCon() {
   });
 }
 
+function isEmpty(obj) {
+  for(var key in obj) {
+      if(obj.hasOwnProperty(key))
+          return false;
+  }
+  return true;
+}
+
 const port = process.env.PORT || 4002;
 const router = express.Router();
 const app = express();
 app.use(express.json());
-app.use(express.urlencoded());
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
@@ -29,7 +36,7 @@ router.get("/", (req, res, next) => {
   next();
 });
 
-router.get("/note/read", (req, res) =>{
+router.get("/note/read", async (req, res) =>{
   var con = createMySqlCon();
   // We will read all of the notes in the database and return it to the user
   console.log('note read request');
@@ -47,6 +54,36 @@ router.get("/note/read", (req, res) =>{
         } else {
           console.log('read all notes successfully');
           res.send({ response: result });          
+        }
+      })
+      console.log('disconnected from sql server');
+      con.end();
+    }
+  })
+})
+
+router.post("/note/read/single", (req, res) =>{
+  var con = createMySqlCon();
+  console.log('single note read request');
+  con.connect((err) => {
+    if (err) {
+      console.log(err);
+      res.send({ response: 'failed to connecting to sql'})
+    } else {
+      // Connected to SQL
+      con.query(`SELECT * FROM noteStorage WHERE noteID='${req.body.key}'`, (err, result, fields) => {
+        if (err){
+          console.log(err);
+          console.log('failed to read a single note');
+          res.send({ response: 'failed to read the notes' });
+        } else {
+          console.log('read the note successfully');
+          if (isEmpty(result)){
+            res.send({ response: 'failed'});
+            console.log('no note');
+          } else {
+            res.send({ response: result }); 
+          }                   
         }
       })
       console.log('disconnected from sql server');
@@ -138,6 +175,7 @@ app.use("/note/insert", router);
 app.use("/note/remove", router);
 app.use("/note/update", router);
 app.use("/note/read", router);
+app.use("/note/read/single", router);
 
 module.exports = router;
 
